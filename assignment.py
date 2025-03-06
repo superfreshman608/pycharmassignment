c4f9f7d326fe498a90ded5a1923b54f298831266
superfreshman <burrujer000@warren.k12.in.us>
2025-03-06 09:14:48 -0500

    initial commit

---
+++ assignment.py
@@ -0,0 +1,9 @@
+my_list = [11, 15, 13, 16, 13, 15, 16, 11]
+print ("The list is: " + str(my_list))
+
+# to remove duplicated from list
+my_list = list(set(my_list))
+
+# printing list after removal
+# ordering distorted
+print ("The list after removing duplicates: " + str(my_list))
